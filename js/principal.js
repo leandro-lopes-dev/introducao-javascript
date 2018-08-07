43675dd11a871263e867e343f36963dee0abdbf1
Leandro Lopes <leandro.lopes.dev@gmail.com>
2018-08-07 10:45:18 -0300

    validações

--- js/principal.js
+++ js/principal.js
@@ -6,13 +6,34 @@
 
 
         var tdPeso = paciente.querySelector(".info-peso");
-        var tdAltura = paciente.querySelector(".info-altura")
-
-
-
         var peso = tdPeso.textContent;
+
+
+        var tdAltura = paciente.querySelector(".info-altura")
         var altura = tdAltura.textContent;
 
-      
-        var imc  = peso / (altura * altura);
-        console.log(imc);
+        var tdImc = paciente.querySelector(".tdImc");
+        var tdImc = paciente.querySelector(".info-imc");
+        
+        var pesoEhValido = true;
+        var alturaEhValida = true;
+        
+        if( peso <= 0 || peso >= 1000){
+                console.log("Peso Inválido");
+                pesoEhValido = false;
+                tdImc.textContent = "Peso Inválido";
+        }
+
+        if (altura <= 0 || altura >= 3.00){
+                console.log ("Altura Inválida");
+                alturaEhValida = false;
+                tdImc.textContent = "Altura Inválida";
+        }
+
+        if(alturaEhValida  && pesoEhValido ){
+                var imc  = peso / (altura * altura);
+               tdImc.textContent = imc;       
+        }
+
+        
+        
