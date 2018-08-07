3965fd349f52a12ef29c803417392f358787f23b
Leandro Lopes <macuser@iMac.local>
2018-08-06 23:00:23 -0300

    página principal.js criada

---
+++ js/principal.js
@@ -0,0 +1,8 @@
+
+        var titulo = (document.querySelector(".titulo"));
+        console.log(titulo);
+        console.log(titulo.textContent);
+
+        titulo.textContent = "Aparecida Nutricionista";
+        console.log("Fui carregado de um arquivo externo");
+     
