bc5ee75717f1d0eb4fae092e47f3577a40c9a217
Leandro Lopes <leandro.lopes.dev@gmail.com>
2018-08-07 13:38:01 -0300

    Arrays, Loops e Estilos

--- js/principal.js
+++ js/principal.js
@@ -2,17 +2,20 @@
         var titulo = (document.querySelector(".titulo"));
         titulo.textContent = "Aparecida Nutricionista";
 
-        var paciente = document.querySelector("#primeiro-paciente");
+        var pacientes = document.querySelectorAll(".paciente");
+       
 
+        for(var i = 0; i < pacientes.length ; i++){
+        var paciente = pacientes[i];
 
         var tdPeso = paciente.querySelector(".info-peso");
         var peso = tdPeso.textContent;
 
 
-        var tdAltura = paciente.querySelector(".info-altura")
+        var tdAltura = paciente.querySelector(".info-altura");
         var altura = tdAltura.textContent;
 
-        var tdImc = paciente.querySelector(".tdImc");
+     
         var tdImc = paciente.querySelector(".info-imc");
         
         var pesoEhValido = true;
@@ -22,18 +25,22 @@
                 console.log("Peso Inválido");
                 pesoEhValido = false;
                 tdImc.textContent = "Peso Inválido";
+                paciente.classList.add("paciente-invalido");
         }
 
         if (altura <= 0 || altura >= 3.00){
                 console.log ("Altura Inválida");
                 alturaEhValida = false;
                 tdImc.textContent = "Altura Inválida";
+                paciente.classList.add("peso-invalido");
         }
 
         if(alturaEhValida  && pesoEhValido ){
                 var imc  = peso / (altura * altura);
-               tdImc.textContent = imc;       
+               tdImc.textContent = imc.toFixed(2);       
         }
 
         
         
+
+        }
